d26b001faeb1da38a4fffdae2c7db7cd95ed5fb4
benbouazza <alexissy@outlook.fr>
2020-02-25 13:39:17 +0100

    test

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var index = require("./routes/index")
+var users = require("./routes/users")
+
+var app = express()
+
+
+app.use(cors())
+
+app.listen(process.env.PORT || 9090);
+
+//Views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine","ejs");
+app.engine("html",require("ejs").renderFile);
+
+//BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Routes
+app.use("/",index);
+app.use("/api",users);
