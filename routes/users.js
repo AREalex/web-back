b9cf64f0c7aa4f10abe07aaffc281064056731dc
benbouazza <alexissy@outlook.fr>
2020-07-06 09:28:47 +0200

    test

--- routes/users.js
+++ routes/users.js
@@ -30,65 +30,15 @@ router.get("/users/:name", function(req,res,next){
 })
 
 router.post("/users", function(req,res,next){
-    var {user, password} = req.body
+    var {user} = req.body
 
     db.users.find({
-        email: user.email,
-        password: user.password
-    },function(err,users){
+        email: user.email
+        },function(err,users){
         if(err){
             res.send(err);
         }
-         /* 2. On envoie une erreur au client si le paramètre username est manquant */
-    if (!username) {
-        return res.status(400).json({ message: 'missing_required_parameter', info: 'username' });
-      }
-      /* 3. On envoie une erreur au client si le paramètre password est manquant */
-      if (!password) {
-        return res.status(400).json({ message: 'missing_required_parameter', info: 'password' });
-      }
-   
-      /* 4. On authentifie l'utilisateur */
-      const user = await User.authenticate(username, password);
-   
-      /* 5. On envoie une erreur au client si les informations de connexion sont erronées */
-      if (!user) {
-        return res.status(401).json({
-          message: 'Username or password is incorrect'
-        });
-      }
-   
-      /* 6. On créer le JWT */
-      const accessToken = await jwt.sign(
-        { firstName: user.firstName, lastName: user.lastName },
-        config.accessToken.secret,
-        {
-          algorithm: config.accessToken.algorithm,
-          audience: config.accessToken.audience,
-          expiresIn: config.accessToken.expiresIn / 1000,
-          issuer: config.accessToken.issuer,
-          subject: user.id.toString()
-        }
-      );
-   
-      /* 7. On créer le refresh token et on le stocke en BDD */
-      const refreshToken = crypto.randomBytes(128).toString('base64');
-   
-      await RefreshToken.create({
-        userId: user.id,
-        token: refreshToken,
-        expiresAt: Date.now() + config.refreshToken.expiresIn
-      });
-   
-      /* 7. On envoie au client le JWT et le refresh token */
-      return res.json({
-        accessToken,
-        tokenType: config.accessToken.type,
-        accessTokenExpiresIn: config.accessToken.expiresIn,
-        refreshToken,
-        refreshTokenExpiresIn: config.refreshToken.expiresIn
-      });
-    } /*
+        
         if(users.length==0){
 
             db.users.insertOne(user,function(err,users){
@@ -108,8 +58,8 @@ router.post("/users", function(req,res,next){
                 res:"incorrect",
                 message:"user already exists"
             }); 
-        } */
-    )
+        }
+    })
 })
 
 module.exports = router;
